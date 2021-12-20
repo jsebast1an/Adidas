610c29a54a831b4b3dc33f97d3077f16d4f11bad
jsebast1an <juanselopezg05@gmail.com>
2021-12-20 19:35:39 -0300

    Form+Modal agregado con exito!

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,12 @@ import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import Formu from "../Formu/Formu";
 import {Link} from "react-router-dom"
-import { BsTrashFill, BsArrowRightCircleFill, BsCheck2 } from "react-icons/bs";
+import { BsTrashFill, BsArrowRightCircleFill } from "react-icons/bs";
 import { useCartContext } from '../../context/CartContext';
 
 function Cart() { 
 
-    const {cartList, removeItem, emptyCart, cartSpan, cartTotal, getOrder} = useCartContext()
+    const {cartList, removeItem, emptyCart, cartSpan, cartTotal} = useCartContext()
     
     return (
         <div>
@@ -51,7 +51,7 @@ function Cart() {
                             </Table>
                             <h4> Total: {cartTotal}  ARS </h4>
                                 <Button className="mx-2" variant="danger" onClick={emptyCart}>Empty cart <BsTrashFill /></Button>
-                                <Button className="mx-2" variant="success" onClick={getOrder}>Order <BsCheck2 /></Button>
+                                
                     </div>
 
 
--- src/components/Formu/Formu.jsx
+++ src/components/Formu/Formu.jsx
@@ -1,34 +1,111 @@
-import Form from 'react-bootstrap/Form'
+import {useState, } from "react"
+import { BsCheck2 } from "react-icons/bs";
+import { getFireStore } from "../../services/getFireStore"
+import { useCartContext } from "../../context/CartContext"
+
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+import firebase from "firebase"
 import "../Formu/Formu.css" 
 
 function Formu() {
+
+    const {cartList, setCartList, cartTotal} = useCartContext()
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [cellphone, setCellphone] = useState("")
+    const [orderId, setOrderId] = useState("")
+
+    const [show, setShow] = useState(false)
+    const handleClose = () => {setShow(false)
+                            setCartList([])}
+                            
+    const order = {}
+
+    function getOrder (e) {
+        e.preventDefault()
+        
+
+        order.buyer = {name, email, cellphone}
+        order.total = cartTotal
+        order.date = firebase.firestore.Timestamp.fromDate( new Date ())
+        order.items = cartList.map( item => {
+            const id = item.id
+            const name = item.nombre
+            const price = item.precio * item.cantidad
+            
+            return {name, price, id}
+        })
+        
+          
+        
+        const fStoreData = getFireStore()
+        fStoreData.collection("orders").add(order)
+        .then(resp => setOrderId(resp))
+        .catch(err=> console.log(err))
+
+        
+        console.log(order)
+
+        setShow(true)
+
+    }
+    
+    console.log(orderId)
+
     return (
         <>
-            <Form>
+            <Form > 
 
                 <Form.Group className="m-3" controlId="formGroupEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={ (e) => setEmail(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group className="m-3" controlId="formGroupPassword">
                     <Row>
                         <Col>
                             <Form.Label>Names</Form.Label>
-                            <Form.Control type="text" placeholder="Names" />
+                            <Form.Control type="text" placeholder="Names" value={name} onChange={ (e) => setName(e.target.value)}/>
                         </Col>
                         <Col>
                             <Form.Label>Cellphone</Form.Label>
-                            <Form.Control type="text" placeholder="Cellphone" />
+                            <Form.Control type="text" placeholder="Cellphone" value={cellphone} onChange={ (e) => setCellphone(e.target.value)}/>
                         </Col>
                     </Row>
                 </Form.Group>
 
+                <Button className="mx-2" variant="success" onClick={getOrder}>Order <BsCheck2 /></Button>
+
             </Form>
+
+            <Modal show={show} onHide={handleClose} className="text-center">
+                <Modal.Header closeButton>
+                <Modal.Title as="h3">Purchase succesfull</Modal.Title>
+                </Modal.Header>
+                <Modal.Body >Your confirmation code: <Modal.Title as="h5"> {orderId.id} </Modal.Title></Modal.Body>
+                <Modal.Body as="p">Thanks for the purchase. <br></br> We will be contacting you via phone or email!</Modal.Body>
+
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                </Button>
+                </Modal.Footer>
+            </Modal>
+
+            
         </>
     )
 }
 
 export default Formu
+
+
+  
